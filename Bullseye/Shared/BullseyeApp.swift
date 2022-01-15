52995ba2bc725d16afd5cfa35f65d98bfa0bb4a6
Juan Camilo Mendieta Hernández <juank.yoh.117@gmail.com>
2022-01-15 15:13:39 -0500

    Creación de proyecto

---
+++ Bullseye/Shared/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Shared
+//
+//  Created by Juan Camilo Mendieta Hernández on 15/01/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Juan Camilo Mendieta Hernández on 15/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
